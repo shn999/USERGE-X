49eb6fa1edf435a849c9453b9141abce8b017975
S A Sohan <50193747+shn999@users.noreply.github.com>
2021-08-01 15:17:15 +0600

    Update config.py

--- userge/config.py
+++ userge/config.py
@@ -167,3 +167,4 @@ def hbot_version(tag: str) -> str:
             except JSONDecodeError:
                 pass
     return f"{tag}|{tag_name or ''}{commits or ''}@{pref_branch or branch or 'alpha'}"
+.
